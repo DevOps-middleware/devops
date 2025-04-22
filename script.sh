a2730eb1c78366edb8ef11885ea1db938f2ce384
vasim <aneeshaanu4578@gmail.com>
2025-04-22 20:13:13 +0530

    commit

--- script.sh
+++ script.sh
@@ -4,8 +4,5 @@ USER=$1
 
 
 if [ -z $USER ]; then
-  echo "user has provided input u can continue"
-  else 
-  echo "user has not provided input u cannot continue"
-  fi
-
+  echo "user has not provided input u cannot cpntinue"
+ fi
