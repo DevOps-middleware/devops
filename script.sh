8e496761368c8cd8aaeed9b06ca3cc1b4d20d0d8
vasim <aneeshaanu4578@gmail.com>
2025-04-22 20:25:51 +0530

    commit

--- script.sh
+++ script.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
-
-NUM=$1
-
-
-  if [ $NUM -lt 10 ] ; then
-  echo " $NUM if lesser than 10"
-  else
-  echo " $NUM is not lesseer tan 10"
+ls -lrth
+if [ $? -eq 0 ]; then
+  echo " installing of nginx is success"
+  else 
+  echo " install of nginx is not success"
   fi
